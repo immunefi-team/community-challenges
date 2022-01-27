598e2d4a565b1eac606659d5e76cac101bb96db6
Duncan Townsend <duncan@immunefi.com>
2022-01-27 14:50:40 -0500

    Use autogenerated accessors

--- contracts/StokenERC20.sol
+++ contracts/StokenERC20.sol
@@ -4,69 +4,43 @@ pragma solidity ^0.8.0;
 import "./interfaces/IERC20.sol";
 
 contract StokenERC20 is IERC20 {
-    mapping(address => uint256) private balances;
-    mapping(address => mapping(address => uint256)) private allowed;
+    mapping(address => uint256) public override balanceOf;
+    mapping(address => mapping(address => uint256)) public override allowance;
 
-    uint256 private _totalSupply;
-    string private _name;
-    string private _symbol;
+    uint256 public override totalSupply;
+    string public constant name = "STOKEN";
+    string public constant symbol = "SETH";
+    uint8 public constant decimals = 18;
 
-    function name() public view returns (string memory) {
-        return _name;
-    }
-
-    function decimals() public pure returns (uint8) {
-        return 18;
-    }
-
-    function totalSupply() public view override returns (uint256) {
-        return _totalSupply;
-    }
-
-
-    function balanceOf(address account) public view override returns (uint256) {
-        return balances[account];
-    }
-
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-
-    constructor(uint _totalSupplyArg) {
-        _name = "STOKEN";
-        _symbol = "SETH";
-        _totalSupply = _totalSupplyArg;
-        balances[msg.sender] = _totalSupply;
+    constructor(uint _totalSupply) {
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
 
     function transfer(address _to, uint _value) public override returns (bool) {
-        if (balances[msg.sender] >= _value && balances[_to] + _value >= balances[_to]) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
+        if (balanceOf[msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]) {
+            balanceOf[msg.sender] -= _value;
+            balanceOf[_to] += _value;
             emit Transfer(msg.sender, _to, _value);
             return true;
         } else { return false; }
     }
 
     function transferFrom(address _from, address _to, uint _value) public override returns (bool) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value >= balances[_to]) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
+        if (balanceOf[_from] >= _value && allowance[_from][msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]) {
+            balanceOf[_to] += _value;
+            balanceOf[_from] -= _value;
             emit Transfer(_from, _to, _value);
+            allowance[_from][msg.sender] -= _value;
+            emit Approval(_from, msg.sender, allowance[_from][msg.sender]);
             return true;
         } else { return false; }
     }
 
     function approve(address _spender, uint _value) public override returns (bool) {
-        allowed[msg.sender][_spender] = _value;
+        allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
-
-    function allowance(address _owner, address _spender) public override view returns (uint) {
-        return allowed[_owner][_spender];
-    }
-
 }
