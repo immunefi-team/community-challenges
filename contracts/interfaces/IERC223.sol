b12bea74fc0edeade898455fbd86f4dff91fd8bd
Duncan Townsend <duncan@immunefi.com>
2022-01-29 17:43:29 -0500

    Autogenerated getters for ERC223

--- contracts/interfaces/IERC223.sol
+++ contracts/interfaces/IERC223.sol
@@ -5,31 +5,30 @@ pragma solidity ^0.8.0;
  * @dev Interface of the ERC223 standard token as defined in the EIP.
  */
 
-abstract contract IERC223 {
+interface IERC223 {
     
-    function name()        public view virtual returns (string memory);
-    function symbol()      public view virtual returns (string memory);
-    function standard()    public view virtual returns (string memory);
-    function decimals()    public view virtual returns (uint8);
-    function totalSupply() public view virtual returns (uint256);
+    function name()        external view returns (string memory);
+    function symbol()      external view returns (string memory);
+    function standard()    external view returns (string memory);
+    function decimals()    external view returns (uint8);
+    function totalSupply() external view returns (uint256);
     
     /**
      * @dev Returns the balance of the `who` address.
      */
-    function balanceOf(address who) public virtual view returns (uint);
+    function balanceOf(address who) external view returns (uint);
         
     /**
      * @dev Transfers `value` tokens from `msg.sender` to `to` address
      * and returns `true` on success.
      */
-    function transfer(address to, uint value) public virtual returns (bool success);
+    function transfer(address to, uint value) external returns (bool success);
         
     /**
      * @dev Transfers `value` tokens from `msg.sender` to `to` address with `data` parameter
      * and returns `true` on success.
      */
-    function transfer(address to, uint value, bytes calldata data) public virtual returns (bool success);
-     
+    function transfer(address to, uint value, bytes calldata data) external returns (bool success);
      /**
      * @dev Event that is fired on successful transfer.
      */
